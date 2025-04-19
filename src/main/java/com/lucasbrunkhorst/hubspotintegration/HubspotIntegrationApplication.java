5d49fcddb06f255536b5578aee240eb91686e561
lucas-brunkhorst <lucasbrunkhorstrosas@gmail.com>
2025-04-19 10:25:58 -0300

    initial commit

---
+++ src/main/java/com/lucasbrunkhorst/hubspotintegration/HubspotIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.lucasbrunkhorst.hubspotintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubspotIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubspotIntegrationApplication.class, args);
+	}
+
+}
